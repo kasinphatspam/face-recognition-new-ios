49679c2cf60b386f0cd9d8208ae96dd233dd3a8c
kasinphatspam <kasinphat@icloud.com>
2023-12-21 00:44:01 +0700

    Initial Commit

---
+++ face-recognition-ios/face_recognition_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  face_recognition_iosApp.swift
+//  face-recognition-ios
+//
+//  Created by Kasinphat Ketchom on 21/12/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct face_recognition_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
